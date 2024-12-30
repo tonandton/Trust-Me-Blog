86943c377e7a41e357186acca1ea6dea9a458557
tonandton <www.sorasakok@gmail.com>
2024-12-30 12:59:40 +0700

    adding group posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
         <div class="w-full text-center py-32">
             <h1 class="text-2xl md:text-3xl font-bold text-center lg:text-5xl text-gray-700">
                 Welcome to <span class="text-emerald-500">&lt;Trust Me&gt;</span>
-                <span class="text-gray-900">News</span>
+                <span class="text-gray-900">Blogs</span>
             </h1>
             <p class="text-gray-500 text-lg mt-1">My Best Blog</p>
             <a href="" class="px-3 py-2 text-lg text-white bg-gray-800 rounded mt-5 inline-block">Start Reading</a>
