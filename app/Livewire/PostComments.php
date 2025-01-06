a9fba21b1741eef9018ed0f0beab12e3d2b18e54
tonandton <www.sorasakok@gmail.com>
2025-01-06 22:52:57 +0700

    fix post comments

--- app/Livewire/PostComments.php
+++ app/Livewire/PostComments.php
@@ -4,12 +4,35 @@ namespace App\Livewire;
 
 use App\Models\Post;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class PostComments extends Component
 {
 
+    use WithPagination;
+
     public Post $post;
+
+    #[Rule('required|min:3|max:200')]
+    public string $comment;
+
+    public function postComment()
+    {
+        if (auth()->guest()) {
+            return;
+        }
+
+        $this->validateOnly('comment');
+
+        $this->post->comments()->create([
+            'comment' => $this->comment,
+            'user_id' => auth()->id()
+        ]);
+
+        $this->reset('comment');
+    }
 
     #[Computed()]
     public function comments()
--- resources/views/livewire/post-comments.blade.php
+++ resources/views/livewire/post-comments.blade.php
@@ -2,10 +2,10 @@
     <h2 class="text-2xl font-semibold text-gray-900 mb-5">Discussions</h2>
 
     @auth
-        <textarea
+        <textarea wire:model="comment"
             class="w-full rounded-lg p-4 bg-gray-50 focus:outline-none text-sm text-gray-700 border-gray-200 placeholder:text-gray-400"
             cols="30" rows="7"></textarea>
-        <button
+        <button wire:click="postComment"
             class="mt-3 inline-flex items-center justify-center h-10 px-4 font-medium tracking-wide text-white transition duration-200 bg-gray-900 rounded-lg hover:bg-gray-800 focus:shadow-outline focus:outline-none">
             Post Comment
         </button>
@@ -17,12 +17,11 @@
         @forelse ($this->comments as $comment)
             <div class="comment [&:not(:last-child)]:border-b border-gray-100 py-5">
                 <div class="user-meta flex mb-4 text-sm items-center">
-                    <img class="w-7 h-7 rounded-full mr-3" src="" alt="mn">
-                    <span class="mr-1">user name</span>
-                    <span class="text-gray-500">. 15 days ago</span>
+                    <x-posts.author :author="$comment->user" size="md" />
+                    <span class="text-gray-500">. {{ $comment->created_at->diffForHumans() }}</span>
                 </div>
                 <div class="text-justify text-gray-700  text-sm">
-                    comment content
+                    {{ $comment->comment }}
                 </div>
             </div>
         @empty
@@ -30,5 +29,10 @@
                 <span> No Comments Posted</span>
             </div>
         @endforelse
+    </div>
+    <div>
+        <div class="my-1">
+            {{ $this->comments->links() }}
+        </div>
     </div>
 </div>
