e7713bdebc875389eb313d6a6f540645eb112408
tonandton <www.sorasakok@gmail.com>
2025-01-17 00:04:46 +0700

    update admin edit delete comment

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\PostResource\Pages;
+use App\Filament\Resources\PostResource\RelationManagers\CommentsRelationManager;
 use App\Models\Post;
 use Filament\Forms;
 use Filament\Forms\Components\Checkbox;
@@ -98,7 +99,7 @@ class PostResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            CommentsRelationManager::class
         ];
     }
 
