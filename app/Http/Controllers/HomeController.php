be0e9e0db069855e4d7807610891d5b8fc6817f2
tonandton <www.sorasakok@gmail.com>
2025-01-19 04:07:52 +0700

    update cache

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
@@ -12,9 +14,18 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+
+        $featuredPosts = Cache::remember('featuredPosts', Carbon::now()->addDay(), function () {
+            return Post::published()->featured()->latest('published_at')->take(3)->get();
+        });
+
+        $latestPosts = Cache::remember('latestPosts', Carbon::now()->addDay(), function () {
+            return Post::published()->latest('published_at')->take(6)->get();
+        });
+
         return view('home', [
-            'featuredPosts' => Post::published()->featured()->latest('published_at')->take(3)->get(),
-            'latestPosts' => Post::published()->latest('published_at')->take(6)->get()
+            'featuredPosts' => $featuredPosts,
+            'latestPosts' => $latestPosts
         ]);
     }
 }
