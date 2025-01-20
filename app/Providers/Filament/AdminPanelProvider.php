46e264cbe8f3d1697adbd0f265284ad92cc48c9b
tonandton <www.sorasakok@gmail.com>
2025-01-20 23:57:14 +0700

    update widget comment

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\CommentResource\Widgets\LatestCommentsWidget;
 use App\Filament\Resources\PostResource\Widgets\PostsPerMonthChart;
 use App\Filament\Resources\UserResource\Widgets\UserStatsWidget;
 use Filament\Http\Middleware\Authenticate;
@@ -40,7 +41,8 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 UserStatsWidget::class,
-                PostsPerMonthChart::class
+                PostsPerMonthChart::class,
+                LatestCommentsWidget::class
             ])
             ->middleware([
                 EncryptCookies::class,
